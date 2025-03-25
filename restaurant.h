0ecebe6defb6f839df2b39680557d9857576b814
LifterSam <119623126+LifterSam@users.noreply.github.com>
2025-03-25 19:52:16 +0100

    Update restaurant.h

--- restaurant.h
+++ restaurant.h
@@ -1,25 +1,23 @@
 #ifndef RESTAURANT_H
 #define RESTAURANT_H
 
-#include "Location.h" // Basisklasse einbinden
+#include "Location.h"
 #include <string>
 
 class Restaurant : public Location
 {
 private:
-    // Neue Attribute für Restaurant
+    // new special attribute for restaurant
     int sitzplaetze_;
 
 public:
-    // Konstruktor
+
     Restaurant(const std::string& name, const std::map<std::string, double>& angebot, int sitzplaetze);
 
-    // Getter und Setter für Sitzplätze
     int getSitzplaetze() const;
     void setSitzplaetze(int sitzplaetze);
 
-    // Methode zum Überschreiben (falls nötig)
     void showAngebot() const override;
 };
 
-#endif // RESTAURANT_H
+#endif
